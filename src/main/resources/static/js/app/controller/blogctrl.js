8ade2b37993125524f9ec34e480d11ff8e2a61ca
Estudiantes <estudiantes@linux102.labinfo.is.escuelaing.edu.co>
2015-11-05 13:08:00 -0500

    actualizacion log

--- src/main/resources/static/js/app/controller/blogctrl.js
+++ src/main/resources/static/js/app/controller/blogctrl.js
@@ -1,7 +1,25 @@
 var app = angular.module("app",[]);
 
-app.controller("blogCtrl", function($scope,$log) {
+app.controller("blogCtrl", function($scope,$log,$http) {
     $scope.entry = {title : "Title",
 			content : "Content"};
+    $scope.entries = [];
     $log.debug('se creo el $scope');
+	$scope.loadData = function() {
+        var configList = {
+                method: "GET",
+                url: "blogs"
+                    };
+
+        var response=$http(configList);
+
+        response.success(function(data, status, headers, config) {
+            $scope.entries = data;
+            });
+
+        response.error(function(data, status, headers, config) {
+            alert("Ha fallado la petición. Estado HTTP:"+status);
+            });
+    };
+    $scope.loadData();
 });
