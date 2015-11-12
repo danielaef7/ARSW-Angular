53402f111562b48d25af52a0b80025c0e3f24c45
danielaef7 <daniesc.07@hotmail.com>
2015-11-12 16:24:19 -0500

    eliminar

--- src/main/resources/static/js/app/controller/blogctrl.js
+++ src/main/resources/static/js/app/controller/blogctrl.js
@@ -2,17 +2,39 @@ var app = angular.module("app",[]);
 
 app.controller("blogCtrl", function($scope,$log,$http) {
     $scope.entry = {title : "Title",
-			content : "Content"};
+                    content : "Content"};
     $scope.entries = [];
     $log.debug('se creo el $scope');
-	$scope.loadData = function() {
+
+ $scope.loadData = function() {
         $http({
-                method: "GET",
-                url: "blogs"
-                }).success(function(data){
-		  $scope.entries = data;
-		}).error(function(data,status,headers,config){
-		  alert("Ha fallado la petición. Estado HTTP:"+status);
-            });
+            method: "GET",
+            url: "blogs"
+        }).success(function(data) {
+            $scope.entries = data;
+        }).error(function(data,status,headers,config) {
+            alert("Ha fallado la petición. Estado HTTP:"+status);
+        });
     };
+
+ $scope.processForm = function() {
+        $log.debug($scope.entry);
+        $http({
+            method  : 'POST',
+            url     : 'blog',
+            data    : $scope.entry
+        }).success(function(data) {
+            console.log(data);
+            $scope.loadData();
+        });
+    };
+ 
+    $scope.delete = function(index) {
+        $scope.entries.splice(index,1);             
+    };
+    
+    /*$scope.edit = function (index) {
+       $scope.entries;
+    };*/
+    
 });
